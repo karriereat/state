e7cfca959592a5d78a4e3f3532e017c496358230
johannes.pichler <johannes.pichler@karriere.at>
2016-11-25 06:49:39 +0100

    add StateFactory for building State objects

---
+++ src/Factories/StateFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Fetzi\State\Factories;
+
+
+use Fetzi\State\State;
+
+class StateFactory
+{
+    public function build($name, $data)
+    {
+        $identifier = md5(uniqid(''));
+
+        return new State($identifier, $name, $data);
+    }
+}
