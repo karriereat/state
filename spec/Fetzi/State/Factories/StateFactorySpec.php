c5d0531e801c5f62a02c7599e564b5c270390f3a
Johannes Pichler <johannes.pichler@jopic.at>
2016-12-06 08:47:50 +0000

    Apply fixes from StyleCI

--- spec/Fetzi/State/Factories/StateFactorySpec.php
+++ spec/Fetzi/State/Factories/StateFactorySpec.php
@@ -7,22 +7,22 @@ use PhpSpec\ObjectBehavior;
 
 class StateFactorySpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType('Fetzi\State\Factories\StateFactory');
     }
 
-    function it_should_assign_a_unique_identifier()
+    public function it_should_assign_a_unique_identifier()
     {
         $this->build('name', [])->shouldHaveType(State::class);
     }
 
-    function it_should_assign_name_to_state()
+    public function it_should_assign_name_to_state()
     {
         $this->build('name', [])->name()->shouldReturn('name');
     }
 
-    function it_should_assign_data_to_state()
+    public function it_should_assign_data_to_state()
     {
         $response = $this->build('name', [1, 2, 3])->raw();
 
--- spec/Fetzi/State/StateSpec.php
+++ spec/Fetzi/State/StateSpec.php
@@ -5,49 +5,48 @@ namespace spec\Fetzi\State;
 use Fetzi\State\State;
 use Illuminate\Support\Collection;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 
 class StateSpec extends ObjectBehavior
 {
-    function let()
+    public function let()
     {
         $this->beConstructedWith('id', 'name', []);
     }
 
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType(State::class);
     }
 
-    function it_should_return_the_identifier()
+    public function it_should_return_the_identifier()
     {
         $this->identifier()->shouldReturn('id');
     }
 
-    function it_should_return_the_name()
+    public function it_should_return_the_name()
     {
         $this->name()->shouldReturn('name');
     }
 
-    function it_should_check_for_the_correct_name()
+    public function it_should_check_for_the_correct_name()
     {
         $this->hasName('foo')->shouldReturn(false);
         $this->hasName('name')->shouldReturn(true);
     }
 
-    function it_should_allow_to_set_data()
+    public function it_should_allow_to_set_data()
     {
         $this->isEmpty()->shouldReturn(true);
         $this->set(['key' => 'value']);
         $this->isEmpty()->shouldReturn(false);
     }
 
-    function it_should_return_raw_array_data()
+    public function it_should_return_raw_array_data()
     {
         $this->raw()->shouldBeArray();
     }
 
-    function it_should_return_collection()
+    public function it_should_return_collection()
     {
         $this->collection()->shouldHaveType(Collection::class);
     }
